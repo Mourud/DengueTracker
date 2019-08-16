5e9d2c4213774921b71fada24c5bc7160c8727df
p2u1b <p2u1b@ugrad.cs.ubc.ca>
2019-08-16 16:28:13 +0600

    BloodPressure documentation completed

--- src/com/company/model/BloodPressure.java
+++ src/com/company/model/BloodPressure.java
@@ -18,6 +18,7 @@ public class BloodPressure {
 
 
     //CONSTRUCTORS
+    // TODO: Is a constructor needed, since there is nothing to be made?
 
     /**
      * Constructs blood pressure data type with systolic and diastolic pressure
@@ -32,29 +33,60 @@ public class BloodPressure {
 
     //SETTERS
 
+    /**
+     * sets systolic and diastolic pressure
+     *
+     * @param systolic  the systolic pressure
+     * @param diastolic the diastolic pressure
+     */
     public void setBloodPressure(int systolic, int diastolic) {
         setSystolic(systolic);
         setDiastolic(diastolic);
     }
 
+    /**
+     * sets systolic pressure
+     *
+     * @param systolic the systolic pressure
+     */
     private void setSystolic(int systolic) {
         this.systolic = systolic;
     }
 
+    /**
+     * sets diastolic pressure
+     *
+     * @param diastolic the diastolic pressure
+     */
     private void setDiastolic(int diastolic) {
         this.diastolic = diastolic;
     }
 
     //GETTERS
 
+    /**
+     * returns systolic pressure
+     *
+     * @return <code>systolic</code>
+     */
     public int getSystolic() {
         return systolic;
     }
 
+    /**
+     * returns diastolic pressure
+     *
+     * @return <code>diastolic</code>
+     */
     public int getDiastolic() {
         return diastolic;
     }
 
+    /**
+     * returns pulse pressure
+     *
+     * @return pulse pressure
+     */
     public int getPulsePressure() {
         return systolic - diastolic;
     }
